e11625a2818d2062f4e2b38157f6b6f83009f2d7
Jose Leon <leonj1@gmail.com>
2016-01-17 14:34:24 -0500

    Comitting solution to match username checker

---
+++ java/src/main/username_checker/Solution.java
@@ -0,0 +1,24 @@
+package username_checker;
+
+/**
+ * Find the regex to match username with these criteria
+ The username can contain alphanumeric characters and/or underscores(_).
+ The username must start with an alphabetic character.
+ 8 ≤ |Username| ≤ 30.
+
+ Examples
+ alpha_naheed
+ xahidbuffon
+ nagib@007
+ 123Swakkhar
+
+ Should result in
+ Valid
+ Valid
+ Invalid
+ Invalid
+ */
+public class Solution {
+    // Regex pattern that worked
+//    String pattern = "^[a-zA-Z]([a-zA-Z|_]+){8,30}";
+}
