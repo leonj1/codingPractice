2371938aa52f9bcad33e87950ad68b3b123673e6
Jose Leon <leonj1@gmail.com>
2016-01-19 00:08:50 -0500

    commenting out to allow me to work on other examples

--- java/src/main/calculating_volume/Calculate.java
+++ java/src/main/calculating_volume/Calculate.java
@@ -1,22 +1,22 @@
-package calculating_volume;
-
-/**
- * Created by jose on 1/17/16.
- */
-public class Calculate {
-    public int getINTVal(){
-        return 0;
-    }
-
-    public double getDoubleVal() {
-        return 1L;
-    }
-
-    static public Volume get_Vol() {
-        return new Cube();
-    }
-
-    static public output() {
-
-    }
-}
+//package calculating_volume;
+//
+///**
+// * Created by jose on 1/17/16.
+// */
+//public class Calculate {
+//    public int getINTVal(){
+//        return 0;
+//    }
+//
+//    public double getDoubleVal() {
+//        return 1L;
+//    }
+//
+//    static public Volume get_Vol() {
+//        return new Cube();
+//    }
+//
+//    static public output() {
+//
+//    }
+//}
--- java/src/main/calculating_volume/Cube.java
+++ java/src/main/calculating_volume/Cube.java
@@ -1,11 +1,11 @@
-package calculating_volume;
-
-/**
- * Created by jose on 1/17/16.
- */
-public class Cube implements Volume {
-    @Override
-    public double main(int length) {
-        return 0;
-    }
-}
+//package calculating_volume;
+//
+///**
+// * Created by jose on 1/17/16.
+// */
+//public class Cube implements Volume {
+//    @Override
+//    public double main(int length) {
+//        return 0;
+//    }
+//}
